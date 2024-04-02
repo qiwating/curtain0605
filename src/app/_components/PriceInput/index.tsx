a4853dfed04b3ec0405378800971d47b9717ed87
qiwating <qiwating@163.com>
2024-04-02 16:30:45 +0800

    4trial

--- src/app/_components/PriceInput/index.tsx
+++
@@ -1,7 +0,0 @@
-// PriceInput component code...
-import React from 'react'
-const PriceInput: React.FC<any> = ({ path, value, onChange }) => {
-  return <input type="number" value={value} onChange={e => onChange(path, e.target.value)} />
-}
-
-export default PriceInput
