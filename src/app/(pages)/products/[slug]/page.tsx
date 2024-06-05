f8398013f562da9d681ecafbe981f82006949543
qiwating <qiwating@163.com>
2024-06-05 09:56:17 +0800

    0605

--- src/app/(pages)/products/[slug]/page.tsx
+++ src/app/(pages)/products/[slug]/page.tsx
@@ -34,11 +34,12 @@ export default async function Product({ params: { slug } }) {
     notFound()
   }
 
-  const { relatedProducts } = product
+  const { layout, relatedProducts } = product
 
   return (
     <>
       <ProductHero product={product} />
+      <Blocks blocks={layout} />
       {product?.enablePaywall && <PaywallBlocks productSlug={slug as string} disableTopPadding />}
       <Blocks
         disableTopPadding
